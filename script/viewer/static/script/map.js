dbf1c14ba10da207ea2729d827615e603b152cac
Drew Roos <mrgriscom@gmail.com>
2015-01-12 15:03:25 +0200

    put child number in icon

--- script/viewer/static/script/map.js
+++ script/viewer/static/script/map.js
@@ -55,6 +55,34 @@ function init($div, data) {
     }, 50);
 }
 
+function singleStyle(props, highlight) {
+    var style;
+    if (props.type == 'peak' || props.type == 'pit') {
+        style = {fillColor: '#f00'};
+    } else if (props.type == 'saddle') {
+        style = {fillColor: '#ff0'};
+    } else if (props.type == 'threshold') {
+        style = {fillColor: '#00f', radius: 5};
+    } else if (props.type == 'child') {
+        style = {fillColor: '#0ff', radius: 8};
+    } else if (props.type == 'parent') {
+        style = {fillColor: '#0f0'};
+    } else if (props.type == 'subsaddle') {
+        style = {fillColor: props.higher ? '#800' : '#000'};
+    } else if (props.type == 'childsaddle') {
+        style = {fillColor: '#00f', radius: 3};
+    }
+    if (highlight) {
+        style.color = '#ff0';
+        style.weight = 3;
+    } else {
+        style.color = '#000';
+        style.weight = 1.5;
+    }
+    return style;
+}
+
+CHILDREN = {};
 OVERLAY = null;
 function loadData(map, data) {
     var overlay = L.geoJson(data, {
@@ -71,25 +99,18 @@ function loadData(map, data) {
         pointToLayer: function(feature, latlng) {
             if (MODE == 'single') {
                 var props = feature.properties;
-                var style;
-                if (props.type == 'peak' || props.type == 'pit') {
-                    style = {fillColor: '#f00'};
-                } else if (props.type == 'saddle') {
-                    style = {fillColor: '#ff0'};
-                } else if (props.type == 'threshold') {
-                    style = {fillColor: '#00f', radius: 5};
-                } else if (props.type == 'child') {
-                    style = {fillColor: '#0ff', radius: 7.5};
-                } else if (props.type == 'parent') {
-                    style = {fillColor: '#0f0'};
-                } else if (props.type == 'subsaddle') {
-                    style = {fillColor: props.higher ? '#800' : '#000'};
-                } else if (props.type == 'childsaddle') {
-                    style = {fillColor: '#00f', radius: 3};
+                if (props.type == 'child') {
+                    var m = L.marker(latlng, {icon: circledNumber(props.ix, 8, '#0ff', 1.5)});
+                } else {
+                    var m = L.circleMarker(latlng, singleStyle(props));
                 }
-                style.color = '#000';
-                style.weight = 1.5;
-                return L.circleMarker(latlng, style);
+                if (props.type == 'child' || props.type == 'childsaddle') {
+                    if (!CHILDREN[props.ix]) {
+                        CHILDREN[props.ix] = {};
+                    }
+                    CHILDREN[props.ix][props.type] = m;
+                }
+                return m;
             } else {
                 var color;
                 if (feature.properties.prom_ft > 4000) {
@@ -102,7 +123,7 @@ function loadData(map, data) {
                     color = 'hsl(120, 100%, 50%)';
                 } else if (feature.properties.prom_ft > 1000) {
                     color = 'hsl(160, 100%, 50%)';
-                } else if (feature.properties.prom_ft > 5000) {
+                } else if (feature.properties.prom_ft > 500) {
                     color = 'hsl(200, 100%, 50%)';
                 } else {
                     color = 'hsl(240, 100%, 50%)';
@@ -134,7 +155,7 @@ function loadData(map, data) {
                         _.each(DATA.features, function(e) {
                             var p = e.properties;
                             if (p.type == 'child') {
-                                html += '<tr><td>' + p.ix + '</td><td><a target="_blank" href="/view/' + p.geo + '">' + (p.name || p.geo) + '</a></td><td>' + dispdist(p, 'prom') + '</td></tr>';
+                                html += '<tr class="childentry" ix="' + p.ix + '"><td>' + p.ix + '</td><td><a target="_blank" href="/view/' + p.geo + '">' + (p.name || p.geo) + '</a></td><td>' + dispdist(p, 'prom') + '</td></tr>';
                             }
                         });
                         html += '</table>';
@@ -156,6 +177,15 @@ function loadData(map, data) {
                     return;
                 }
                 layer.bindPopup($div[0]);
+
+                var highlightChild = function(e, highlight) {
+                    var ix = +$(e.currentTarget).attr('ix');
+                    $.each(CHILDREN[ix], function(k, v) {
+                        v.setStyle(singleStyle(v.feature.properties, highlight));
+                    });
+                }
+                //$div.find('tr.childentry').mouseenter(function(e) { highlightChild(e, true) });
+                //$div.find('tr.childentry').mouseleave(function(e) { highlightChild(e, false) });
             } else {
                 var $div = $('<div>');
                 $div.html('<div><a target="_blank" href="/view/' + props.geo + '">' + (props.name || props.geo) + '</a></div><div>' + dispdist(props, 'prom') + (props.min_bound ? '*' : '') + '</div><div>' + dispdist(props, 'elev') + '</div>');
@@ -182,4 +212,46 @@ function dispdist(obj, field) {
     var m = obj[field + '_m'];
     var ft = obj[field + '_ft'];
     return '<span style="white-space: nowrap;">' + ft.toFixed(1) + ' ft | ' + m.toFixed(1) + ' m' + '</span>';
+}
+
+function mk_canvas(w, h) {
+    var $c = $('<canvas />');
+    $c.attr('width', w);
+    $c.attr('height', h);
+    var c = $c[0];
+    var ctx = c.getContext('2d');
+    return {canvas: c, context: ctx};
+}
+
+function circledNumber(n, radius, fillColor, strokeWidth) {
+    var dim = Math.ceil(2 * (radius + .5 * strokeWidth + 2));
+    var W = dim;
+    var H = dim;
+    var c = mk_canvas(W, H);
+
+    c.context.globalAlpha = 0.5;
+    c.context.beginPath();
+    c.context.arc(W/2, H/2, radius, 0, 2 * Math.PI, false);
+    c.context.fillStyle = fillColor;
+    c.context.fill();
+    c.context.lineWidth = strokeWidth;
+    c.context.strokeStyle = 'black';
+    c.context.stroke();
+
+    c.context.globalAlpha = 1.;
+    c.context.textAlign = 'center';
+    c.context.textBaseline = 'middle';
+    if (n < 10) {
+        var size = 10;
+    } else if (n < 100) {
+        var size = 7;
+    } else {
+        var size = 5;
+    }
+    c.context.font = size + 'pt sans-serif';
+    c.context.fillStyle = 'black';
+    c.context.fillText(n, W/2, H/2);
+
+    var url = c.canvas.toDataURL();
+    return L.icon({iconUrl: url, iconSize: [W, H]});
 }
