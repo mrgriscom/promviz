8f25d8d4b8ea13956c41613fc3458db7ff49ba31
Drew Roos <mrgriscom@gmail.com>
2015-04-24 00:52:22 -0400

    process pending

--- promviz/src/promviz/TopologyBuilder.java
+++ promviz/src/promviz/TopologyBuilder.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -14,6 +16,7 @@ import old.promviz.util.Logging;
 import promviz.MeshPoint.Lead;
 import promviz.dem.DEMFile;
 import promviz.dem.SRTMDEM;
+import promviz.util.DefaultMap;
 import promviz.util.WorkerPool;
 import promviz.util.WorkerPoolDebug;
 
@@ -73,7 +76,7 @@ public class TopologyBuilder {
 		
 		PagedElevGrid mesh;
 		List<ChaseResult> processed;
-		List<ChaseResult> pending;
+		List<Lead> pending; // TODO group by common (term, up)?
 		Set<Long> checkpoints;
 		
 		public ChunkProcessor(ChunkInput input) {
@@ -81,7 +84,7 @@ public class TopologyBuilder {
 			this.coverage = input.coverage;
 			
 			processed = new ArrayList<ChaseResult>();
-			pending = new ArrayList<ChaseResult>();
+			pending = new ArrayList<Lead>();
 			checkpoints = new HashSet<Long>();
 		}
 		
@@ -102,6 +105,10 @@ public class TopologyBuilder {
 				}
 				
 				processSaddle(p);
+			}
+			
+			while (pending.size() > 0) {
+				processPending();
 			}
 			
 			Logging.log(prefix + " " + processed.size() + " " + pending.size());
@@ -131,8 +138,17 @@ public class TopologyBuilder {
 					}
 				}
 			} else {
-				// pending
-				pending.add(result);
+				pending.add(result.lead);
+			}
+		}
+	
+		void processPending() {
+			mesh.loadPage(bestNextPage());
+			
+			List<Lead> pending_ = pending;
+			pending = new ArrayList<Lead>();
+			for (Lead pend : pending_) {
+				processLead(pend);
 			}
 		}
 		
@@ -207,6 +223,58 @@ public class TopologyBuilder {
 			return !prefix.isParent(p.ix) || checkpoints.contains(p.ix);
 		}
 		
+		Prefix bestNextPage() {
+			final Map<Prefix, Double> opportunity = new DefaultMap<Prefix, Double>() {
+				public Double defaultValue(Prefix key) {
+					return 0.;
+				}
+			};
+			final Map<Prefix, Integer> demand = new DefaultMap<Prefix, Integer>() {
+				public Integer defaultValue(Prefix key) {
+					return 0;
+				}				
+			};
+			
+			for (Lead pend : pending) {
+				MeshPoint p = pend.p;
+				Set<Prefix> frontiers = new HashSet<Prefix>();
+				frontiers.add(PagedElevGrid.segmentPrefix(p.ix));
+				for (long adj : p.adjIx()) {
+					frontiers.add(PagedElevGrid.segmentPrefix(adj));
+				}
+				
+				int numLoaded = 0;
+				for (Prefix page : frontiers) {
+					if (mesh.isLoaded(page)) {
+						numLoaded++;
+					}
+				}
+				int numNeeded = frontiers.size() - numLoaded;
+
+				double opp = (double)frontiers.size() / numNeeded;
+				for (Prefix page : frontiers) {
+					if (!mesh.isLoaded(page)) {
+						opportunity.put(page, Math.max(opportunity.get(page), opp));
+						demand.put(page, demand.get(page) + 1);
+					}
+				}
+			}
+			
+			return Collections.max(demand.keySet(), new Comparator<Prefix>() {
+				public int compare(Prefix p1, Prefix p2) {
+					double opp1 = opportunity.get(p1);
+					double opp2 = opportunity.get(p2);
+					int demand1 = demand.get(p1);
+					int demand2 = demand.get(p2);
+					
+					if (opp1 != opp2) {
+						return Double.valueOf(opp1).compareTo(opp2);
+					} else {
+						return Integer.valueOf(demand1).compareTo(demand2);
+					}
+				}
+			});
+		}
 	}
 	
 	
@@ -216,136 +284,13 @@ public class TopologyBuilder {
 	}
 
 	
+
 	
-//
-//	public void tallyPending(Set<Prefix> allPrefixes, Map<Set<Prefix>, Integer> frontierTotals) {
-//		for (Lead lead : pendingLeads) {
-//			long term = lead.p.ix;
-//			boolean interior = tallyAdjacency(term, allPrefixes, frontierTotals);
-//			if (interior) {
-//				pendInterior.add(term);
-//			} else {
-//				writePendingLead(lead);
-//			}
-//		}
-//		
-//		List<Long> edgeFringe = new ArrayList<Long>();
-//		for (long fringe : unprocessedFringe) {
-//			boolean interior = tallyAdjacency(fringe, allPrefixes, frontierTotals);
-//			if (interior) {
-//				pendInterior.add(fringe);
-//			} else {
-//				edgeFringe.add(fringe);
-//			}
-//		}
-//		unprocessedFringe.removeAll(edgeFringe);
-//
-//		// trim pendInterior to only what is still relevant
-//		Set<Long> newPendInterior = new HashSet<Long>();
-//		for (Lead lead : pendingLeads) {
-//			long term = lead.p.ix;
-//			if (pendInterior.contains(term)) {
-//				newPendInterior.add(term);
-//			}
-//		}
-//		for (long fringe : unprocessedFringe) {
-//			if (pendInterior.contains(fringe)) {
-//				newPendInterior.add(fringe);
-//			}
-//		}
-//		pendInterior = newPendInterior;
-//	}
-//
-//	Prefix matchPrefix(long ix, Set<Prefix> prefixes) {
-//		Prefix p = new Prefix(ix, DEMManager.GRID_TILE_SIZE);
-//		return (prefixes.contains(p) ? p : null);
-//	}
-//	
-//	boolean tallyAdjacency(long ix, Set<Prefix> allPrefixes, Map<Set<Prefix>, Integer> totals) {
-//		Set<Prefix> frontiers = new HashSet<Prefix>();
-//		frontiers.add(matchPrefix(ix, allPrefixes));
-//		for (long adj : DEMManager.adjacency(ix)) {
-//			// find the parititon the adjacent point lies in
-//			Prefix partition = matchPrefix(adj, allPrefixes);
-//
-//			if (!pendInterior.contains(ix)) {
-//				if (partition == null) {
-//					// point is adjacent to a point that will never be loaded, i.e., on the edge of the
-//					// entire region of interest; it will be indeterminate forever
-//					return false;
-//				}
-//				if (!dm.inScope(adj)) {
-//				//i think this is the only required check; the prefix-matching above can be discarded
-//					return false;
-//				}
-//			}
-//				
-//			frontiers.add(partition);
-//		}
-//		
-//		totals.put(frontiers, totals.get(frontiers) + 1);
-//		return true;
-//	}
-//	
-//
-//		
-//		
-//		Set<Lead> oldPending = pendingLeads;
-//		pendingLeads = new HashSet<Lead>();
-//		pendingSaddles = new HashSet<Point>();
-//		for (Lead lead : oldPending) {
-//			Point saddle = lead.p0;
-//			Point head = m.get(lead.p.ix);
-//			if (head == null) {
-//				// point not loaded -- effectively indeterminate
-//				addPending(lead); // replicate entry in new map
-//			} else {
-//				processLead(m, lead);
-//			}
-//		}
-//		
-//		Logging.log("# pending: " + oldPending.size() + " -> " + pendingLeads.size());
-//		Set<Long> fringeNowProcessed = new HashSet<Long>();
-//		for (long ix : unprocessedFringe) {
-//			// TODO don't reprocess points that were also pending leads?
-//			// arises when a saddle leads to another saddle
-//			Point p = m.get(ix);
-//			int pointClass = (p != null ? p.classify(m) : Point.CLASS_INDETERMINATE);
-//			if (pointClass != Point.CLASS_INDETERMINATE) {
-//				fringeNowProcessed.add(ix);
-//				if (pointClass == Point.CLASS_SADDLE) {
-//					processSaddle(m, p);
-//				}
-//			}
-//		}
-//		unprocessedFringe.removeAll(fringeNowProcessed);
-//		Logging.log("# fringe: " + (unprocessedFringe.size() + fringeNowProcessed.size()) + " -> " + unprocessedFringe.size());
-//
-//		if (newPage != null) {
-//			build(m, newPage);
-//		}
-//		
-//		trimNetwork();
-//	}
-//	
-//	void trimNetwork() {
-//		List<Point> toRemove = new ArrayList<Point>();
-//		for (Point p : points.values()) {
-//			if (!pendingSaddles.contains(p)) {
-//				toRemove.add(p);
-//			}
-//		}
-//		for (Point p : toRemove) {
-//			points.remove(p);
-//		}
-//
-//		
-//		
-//		
-//		
-//	
-//	
-//	
+
+	
+
+
+
 //	
 	public static List<DEMFile> loadDEMs(String region) {
 		List<DEMFile> dems = new ArrayList<DEMFile>();
@@ -383,14 +328,6 @@ saddle->summit pairs for all saddles within chunk boundary. summit may be inside
 list of checkpoints created inside chunk boundary
 list of checkpoints terminated at outside chunk boundary
 
-bulk load all pages in chunk + one page fringe around border
-for all points in chunk, identify saddles
-for any saddles, for each lead (up+down) trace until local max/min. checkpointing rules in effect.
-if hit edge of loaded area, pend
-pend is uniq by lead (multiple pending saddles for same lead, potentially)
-
-we only bulk process initially loaded area
-once pages are being loaded on-demand, it is to only chase down pending leads
 
 		 * 
 		 * 
@@ -402,130 +339,7 @@ once pages are being loaded on-demand, it is to only chase down pending leads
 	
 	
 	
-//	
-//	
-//	DualTopologyNetwork buildAll() {
-//		Map<Prefix, Set<DEMFile>> coverage = this.partitionDEM();
-//		Set<Prefix> allPrefixes = coverage.keySet();
-//		Logging.log("partitioning complete");
-//		Set<Prefix> yetToProcess = new HashSet<Prefix>(coverage.keySet()); //mutable!
-//
-//		PagedElevGrid m = new PagedElevGrid(coverage, MESH_MAX_POINTS);
-//		DualTopologyNetwork tn = new DualTopologyNetwork(this, true);
-//		while (!tn.complete(allPrefixes, yetToProcess)) {
-//			Prefix nextPrefix = getNextPrefix(allPrefixes, yetToProcess, tn, m);
-//			if (m.isLoaded(nextPrefix)) {
-//				Logging.log("prefix already loaded!");
-//				continue;
-//			}
-//			
-//			Iterable<DEMFile.Sample> newData = m.loadPage(nextPrefix);
-//			tn.buildPartial(m, yetToProcess.contains(nextPrefix) ? newData : null);
-//			yetToProcess.remove(nextPrefix);
-//			Logging.log(yetToProcess.size() + " ytp");
-//		}
-//		return tn;
-//	}
 //		
-//	Prefix getNextPrefix(Set<Prefix> allPrefixes, Set<Prefix> yetToProcess, TopologyNetwork tn, PagedElevGrid m) {
-//		Map<Set<Prefix>, Integer> frontierTotals = tn.tallyPending(allPrefixes);
-//		
-//		Map<Prefix, Set<Set<Prefix>>> pendingPrefixes = new DefaultMap<Prefix, Set<Set<Prefix>>>() {
-//			@Override
-//			public Set<Set<Prefix>> defaultValue(Prefix _) {
-//				return new HashSet<Set<Prefix>>();
-//			}			
-//		};
-//		for (Set<Prefix> prefixGroup : frontierTotals.keySet()) {
-//			for (Prefix p : prefixGroup) {
-//				pendingPrefixes.get(p).add(prefixGroup);
-//			}
-//		}
-//		
-//		Prefix mostInDemand = null;
-//		int bestScore = 0;
-//		for (Entry<Prefix, Set<Set<Prefix>>> e : pendingPrefixes.entrySet()) {
-//			Prefix p = e.getKey();
-//			Set<Set<Prefix>> cohorts = e.getValue();
-//
-//			Logging.log(String.format("pending> %s...", e.getKey())); // more?
-//
-//			if (m.isLoaded(p)) {
-//				continue;
-//			}
-//			
-//			int score = 0;
-//			for (Set<Prefix> cohort : cohorts) {
-//				int numLoaded = 0;
-//				for (Prefix coprefix : cohort) {
-//					if (coprefix != p && m.isLoaded(coprefix)) {
-//						numLoaded++;
-//					}
-//				}
-//				int cohortScore = 1000000 * numLoaded + frontierTotals.get(cohort);
-//				score = Math.max(score, cohortScore);
-//			}
-//			
-//			if (score > bestScore) {
-//				bestScore = score;
-//				mostInDemand = p;
-//			}
-//		}
-//		
-//		if (mostInDemand == null) {
-//			mostInDemand = yetToProcess.iterator().next();
-//		}
-//		return mostInDemand;
-//	}
-//		
-//	class PartitionCounter {
-//		int count;
-//		Set<DEMFile> coverage;
-//		
-//		public PartitionCounter() {
-//			count = 0;
-//			coverage = new HashSet<DEMFile>();
-//		}
-//		
-//		public void addSample(DEMFile dem) {
-//			count++;
-//			coverage.add(dem);
-//		}
-//		
-//		public void combine(PartitionCounter pc) {
-//			count += pc.count;
-//			coverage.addAll(pc.coverage);
-//		}
-//	}
-//		
-//	// HACKY
-//	boolean inScope(long ix) {
-//		// FIXME bug lurking here: nodata areas within loaded DEM extents
-//		int[] _ix = PointIndex.split(ix);
-//		int[] xy = {_ix[1], _ix[2]};
-//		for (DEMFile dem : DEMs) {
-//			if (xy[0] >= dem.x0 && xy[1] >= dem.y0 &&
-//					xy[0] < (dem.x0 + dem.height) &&
-//					xy[1] < (dem.y0 + dem.width)) {
-//				return true;
-//			}
-//		}
-//		return false;
-//	}
-//
-//	
-//	static void buildTopologyNetwork(DEMManager dm, String region) {
-//		loadDEMs(dm, region);
-//		
-//		DualTopologyNetwork dtn;
-//		MESH_MAX_POINTS = Long.parseLong(props.getProperty("memory")) / 8;
-//		dtn = dm.buildAll();
-//		dtn.up.cleanup();
-//		dtn.down.cleanup();
-//		
-//		System.err.println("edges in network (up), " + dtn.up.numEdges);
-//		System.err.println("edges in network (down), " + dtn.down.numEdges);
-//	}
 //	
 //	static void preprocessNetwork(DEMManager dm, String region) {
 //		if (region != null) {
@@ -619,57 +433,7 @@ once pages are being loaded on-demand, it is to only chase down pending leads
 //		}
 //	}
 //
-//	public class DualTopologyNetwork extends TopologyNetwork {
-//
-//		TopologyNetwork up;
-//		TopologyNetwork down;
 //		
-//		public DualTopologyNetwork(DEMManager dm, boolean cache) {
-//			up = new TopologyNetwork(true, dm);
-//			down = new TopologyNetwork(false, dm);
-//			if (cache) {
-//				up.enableCache();
-//				down.enableCache();
-//			}
-//		}
-//		
-//		public DualTopologyNetwork() {
-//			
-//		}
-//		
-//		public static DualTopologyNetwork load(DEMManager dm) {
-//			DualTopologyNetwork dtn = new DualTopologyNetwork();
-//			dtn.up = new PagedTopologyNetwork(EdgeIterator.PHASE_RAW, true, dm, null);
-//			dtn.down = new PagedTopologyNetwork(EdgeIterator.PHASE_RAW, false, dm, null);
-//			return dtn;
-//		}
-//		
-//		public void buildPartial(PagedElevGrid m, Iterable<DEMFile.Sample> newPage) {
-//			long start = System.currentTimeMillis();
-//			
-//			// seems a bit questionable to use an iterable twice like this...
-//			up.buildPartial(m, newPage);
-//			down.buildPartial(m, newPage);
-//
-//			Logging.log("@buildPartial: " + (System.currentTimeMillis() - start));
-//		}
-//
-//		public Map<Set<Prefix>, Integer> tallyPending(Set<Prefix> allPrefixes) {
-//			long start = System.currentTimeMillis();
-//					
-//			Map<Set<Prefix>, Integer> frontierTotals = new DefaultMap<Set<Prefix>, Integer>() {
-//				@Override
-//				public Integer defaultValue(Set<Prefix> _) {
-//					return 0;
-//				}
-//			};
-//			up.tallyPending(allPrefixes, frontierTotals);
-//			down.tallyPending(allPrefixes, frontierTotals);
-//
-//			Logging.log("@tallyPending: " + (System.currentTimeMillis() - start));
-//			
-//			return frontierTotals;
-//		}
 //
 //
 //	}
