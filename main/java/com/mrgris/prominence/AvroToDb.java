c54e13ff2a246830d88ab01b6f6aebdae2e43143
Drew Roos <mrgriscom@gmail.com>
2018-03-04 18:49:55 +0200

    spatialite support

--- main/java/com/mrgris/prominence/AvroToDb.java
+++ main/java/com/mrgris/prominence/AvroToDb.java
@@ -9,10 +9,12 @@ import java.sql.Statement;
 import org.apache.avro.file.DataFileReader;
 import org.apache.avro.io.DatumReader;
 import org.apache.avro.reflect.ReflectDatumReader;
+import org.sqlite.SQLiteConfig;
 
 import com.mrgris.prominence.Prominence.PromFact;
+import com.mrgris.prominence.util.GeoCode;
 
-// support spatialite
+// use jts for building wkt's
 
 public class AvroToDb {
 	
@@ -22,16 +24,22 @@ public class AvroToDb {
 		
 		try {
 
-	        Connection conn = DriverManager.getConnection("jdbc:sqlite:" + dbname);
+            SQLiteConfig config = new SQLiteConfig();
+            config.enableLoadExtension(true);
+	        Connection conn = DriverManager.getConnection("jdbc:sqlite:" + dbname, config.toProperties());
 
             Statement stmt = conn.createStatement();
-            stmt.execute("create table points (ix int64 primary key);");
-
+            stmt.execute("SELECT load_extension('mod_spatialite')");
+            stmt.execute("SELECT InitSpatialMetadata(1)");
+                        
+            stmt.execute("create table points (geocode int64 primary key);");
+            stmt.execute("SELECT AddGeometryColumn('points', 'loc', 4326, 'POINT', 'XY')");
+            
             conn.setAutoCommit(false);
             int batchSize = 10000;            
-            String sqlInsert = "insert into points values (?)";
+            String sqlInsert = "insert into points values (?,GeomFromText(?, 4326))";
             PreparedStatement ps = conn.prepareStatement(sqlInsert);
-
+            
 			DatumReader<PromFact> userDatumReader = new ReflectDatumReader<PromFact>(PromFact.class);
 			DataFileReader<PromFact> dataFileReader = new DataFileReader<PromFact>(new File(pipelineOutput), userDatumReader);
 			PromFact pf = null;
@@ -39,8 +47,13 @@ public class AvroToDb {
 			int i = 0;
 			while (dataFileReader.hasNext()) {
 				pf = dataFileReader.next(pf);
+
+				long ix = pf.p.ix;
+				double coords[] = PointIndex.toLatLon(ix);
+				long geo = GeoCode.fromCoord(coords[0], coords[1]);
 				
-	            ps.setLong(1, pf.p.ix);
+	            ps.setLong(1, geo);
+	            ps.setString(2, String.format("POINT(%f %f)", coords[1], coords[0]));
 	            ps.addBatch();
 	            if (i % batchSize == 0) {
 	            	int[] counts = ps.executeBatch();
