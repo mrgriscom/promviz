314f0d1cb469342a335039f8c5e1f579a59bc98b
Drew Roos <mrgriscom@gmail.com>
2018-03-14 19:11:09 +0200

    clean up viewer for sqlite db

--- script/viewer/server.py
+++ script/viewer/server.py
@@ -11,208 +11,230 @@ import sys
 import re
 import collections
 import math
+import sqlite3
+import geojson
+import shapely.wkt
+import itertools
 
 import os.path
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import settings
 import util
 
-meat = lambda k: k[k['type']]
+def fetch_prom(where_clause, args):
+    results = conn.execute('select point, saddle, prom_mm, min_bound, prom_parent, line_parent from prom where %s' % where_clause, args)
+    def to_rec(row):
+        return {
+            'point': geo_itos(row[0]),
+            'saddle': geo_itos(row[1]),
+            'prom': row[2]/1000.,
+            'min_bound': bool(row[3]),
+            'parent': tx(geo_itos, row[4]),
+            'pthresh': tx(geo_itos, row[5]),
+        }
+    return map(to_rec, results)
+
+def fetch_points(ids):
+    ids = set(ids)
+    results = conn.execute('select geocode, type, elev_mm, AsWkt(loc) from points where geocode in (%s)' % ','.join(['?']*len(ids)), map(geo_stoi, ids))
+    def to_rec(row):
+        return {
+            'geo': geo_itos(row[0]),
+            'type': {1: 'peak', -1: 'pit', 0: 'saddle'}[row[1]],
+            'elev': row[2]/1000.,
+            'coords': row[3],
+        }
+    return dict((rec['geo'], rec) for rec in map(to_rec, results))
+
+def fetch_subsaddles(point):
+    results = conn.execute('select saddle, is_elev, is_prom from subsaddles where point = ?', (geo_stoi(point),))
+    def to_rec(row):
+        return {
+            'saddle': geo_itos(row[0]),
+            'elev': bool(row[1]),
+            'prom': bool(row[2]),
+        }
+    return map(to_rec, results)
+
+def build_prom_info(proms):
+    proms = list(proms)
+    return {
+        'proms': dict((p['point'], p) for p in proms),
+        'proms_by_saddle': dict((p['saddle'], p) for p in proms),
+        'points': fetch_points(prom[k] for k in ('point', 'saddle') for prom in proms),
+    }
 
 def _feature(coords, **props):
-    def coord(c):
-        return [c[1], c[0]]
-
-    if hasattr(coords[0], '__iter__'):
-        if hasattr(coords[0][0], '__iter__'):
-            type = 'MultiLineString'
-            coords = [[coord(c) for c in k] for k in coords]
-        else:
-            type = 'LineString'
-            coords = [coord(c) for c in coords]
-    else:
-        type = 'Point'
-        coords = coord(coords)
-
     return {
         'type': 'Feature',
-        'geometry': {
-            'type': type,
-            'coordinates': coords,
-        },
+        'geometry': geojson.Feature(geometry=shapely.wkt.loads(coords)).geometry,
         'properties': props,
     }
 
-def summit_feature(s, **props):
-    f = {
-        'type': 'Feature',
-        'geometry': {
-            'type': 'Point',
-            'coordinates': [s['coords'][1], s['coords'][0]],
-        },
-        'properties': {
-            'type': s.get('type'),
-            'name': s.get('name'),
-            'prom_m': s['prom'],
-            'elev_m': s['elev'],
-            'prom_ft': s['prom'] / .3048,
-            'elev_ft': s['elev'] / .3048,
-            'min_bound': s.get('min_bound', False),
-            'geo': s['geo'],
-        },
+def summit_feature(info, geo, **props):
+    prom = info['proms'][geo]
+    point = info['points'][geo]
+
+    _props = {
+        'type': point.get('type'),
+        #'name': s.get('name'),
+        'prom_m': prom['prom'],
+        'elev_m': point['elev'],
+        'prom_ft': prom['prom'] / .3048,
+        'elev_ft': point['elev'] / .3048,
+        'min_bound': prom.get('min_bound', False),
+        'geo': geo,
     }
-    f['properties'].update(props)
-    return f
+    _props.update(props)
+    return _feature(point['coords'], **_props)
 
-def saddle_feature(k, type='saddle', **props):
-    f = _feature(k['saddle']['coords'],
-                 type=type,
-                 name=k['saddle'].get('name'),
-                 elev_m=k['saddle']['elev'],
-                 elev_ft=k['saddle']['elev'] / .3048,
-                 geo=k['saddle']['geo'],
-                 )
-    if k.get('_for'):
-        props['peak'] = summit_feature(meat(k['_for']), type='sspeak')
-    f['properties'].update(props)
-    return f
+def saddle_feature(info, geo, type='saddle', ref_peak=False, **props):
+    point = info['points'][geo]
+    prom = info['proms_by_saddle'][geo]
+    
+    if ref_peak:
+        props['peak'] = summit_feature(info, prom['point'], type='sspeak')
+    props.update({
+        'type': type,
+        #'name': s.get('name'),
+        'elev_m': point['elev'],
+        'elev_ft': point['elev'] / .3048,
+        'geo': geo,
+    })
+    return _feature(point['coords'], **props)
 
-def to_geojson(k):
-    data = {
-        'type': 'FeatureCollection',
-        'features': [
-            summit_feature(k[k['type']], type=k['type']),
-            saddle_feature(k),
-            _feature(k['threshold_path'], type='divide'),
-        ]
-    }
-    for ss in k.get('height_subsaddles', []):
-        data['features'].append(saddle_feature(ss, 'subsaddle', higher=True, domain=False))
-    for ss in k.get('prom_subsaddles', []):
-        data['features'].append(saddle_feature(ss, 'subsaddle', higher=False, domain=True))
+def geo_stoi(s):
+    i = int(s, 16)
+    if i >= 2**63:
+        i -= 2**64
+    return i
 
-    if k.get('threshold'):
-        data['features'].append(
-            _feature(k['threshold']['coords'],
-                     type='threshold',
-                 ),
-        )
-    if k.get('parent'):
-        data['features'].extend([
-            summit_feature(meat(k['_parent']), type='parent'),
-            _feature(k['parent_path'], type='toparent'),
-        ])
-    for child in k.get('_children', []):
-        data['features'].append(summit_feature(meat(child), type='child', ix=child['ix']))
-        data['features'].append(saddle_feature(child, type='childsaddle', ix=child['ix']))
-    if k.get('pthresh'):
-        data['features'].append(summit_feature(meat(k['_pthresh']), type='pthresh'))
+def geo_itos(i):
+    if i < 0:
+        i += 2**64
+    return '%016x' % i
 
-    if k.get('runoff'):
-        data['features'].extend(_feature(t, type='domain') for t in k['runoff'])
-    for child in k.get('_children', []):
-        if child.get('runoff'):
-            data['features'].extend(_feature(t, type='child-domain') for t in child['runoff'])
-
-    return data
+def tx(func, val):
+    return func(val) if val is not None else None
 
 class MapViewHandler(web.RequestHandler):
     def get(self, tag):
-        def loadgeo(geo):
-            with open(os.path.join(settings.dir_out, 'prom%s.json' % geo)) as f:
-                return json.load(f)
-        
-        data = loadgeo(tag)
+        main = fetch_prom('point = ?', (geo_stoi(tag),))[0]
+        parents = fetch_prom('point in (?,?)', (tx(geo_stoi, main['parent']), tx(geo_stoi, main['pthresh'])))
+        children = fetch_prom('prom_parent = ?', (geo_stoi(tag),))
+        subsaddle_peaks = fetch_prom('saddle in (select saddle from subsaddles where point = ?)', (geo_stoi(tag),))
 
-        if 'runoff' not in data:
-            print 'tracing runoff...'
-            os.popen('python ../runoff.py %s' % tag)
-            data = loadgeo(tag)
+        info = build_prom_info(itertools.chain([main], parents, children, subsaddle_peaks))
+        subsaddles = fetch_subsaddles(tag)
 
-        data['_parent'] = loadgeo(data['parent']['geo']) if 'parent' in data else None
-        data['_children'] = [loadgeo(c) for c in data.get('children', [])]
-
-        for i, ch in enumerate(data['_children']):
-            if 'runoff' not in ch:
-                print 'tracing child runoff...'
-                tag = meat(ch)['geo']
-                os.popen('python ../runoff.py %s' % tag)
-                data['_children'][i] = loadgeo(tag)
-
-        for i, ch in enumerate(data['_children']):
-            ch['ix'] = i + 1
-        for sstype in ('height', 'prom'):
-            for ss in data.get('%s_subsaddles' % sstype, []):
-                ss['_for'] = loadgeo(ss['for']['geo'])
-        data['_pthresh'] = loadgeo(data['pthresh']['geo']) if 'pthresh' in data else None        
-
-        self.render('map.html', mode='single', data=to_geojson(data))
+        data = {
+            'type': 'FeatureCollection',
+            'features': [
+                summit_feature(info, tag, type='peak'),
+                saddle_feature(info, main['saddle']),
+                #_feature(k['threshold_path'], type='divide'), #also type 'threshold'?
+            ]
+        }
+        for ss in subsaddles:
+            if ss['elev']:
+                data['features'].append(saddle_feature(info, ss['saddle'], 'subsaddle', ref_peak=True, domain=False))
+            if ss['prom']:
+                data['features'].append(saddle_feature(info, ss['saddle'], 'subsaddle', ref_peak=True, domain=True))    
+        if main['parent'] is not None:
+            data['features'].extend([
+                summit_feature(info, main['parent'], type='parent'),
+                #_feature(k['parent_path'], type='toparent'),
+            ])
+        for i, ch in enumerate(sorted(children, key=lambda prom: -prom['prom'])):
+            ix = i + 1
+            data['features'].extend([
+                summit_feature(info, ch['point'], type='child', ix=ix),
+                # better to replace with child paths to parent + child domains
+                saddle_feature(info, ch['saddle'], type='childsaddle', ix=ix)
+            ])
+        if main['pthresh'] is not None:
+            data['features'].append(
+                summit_feature(info, main['pthresh'], type='pthresh'),
+            )
+    
+        #if k.get('runoff'):
+        #    data['features'].extend(_feature(t, type='domain') for t in k['runoff'])
+        #for child in k.get('_children', []):
+        #    if child.get('runoff'):
+        #        data['features'].extend(_feature(t, type='child-domain') for t in child['runoff'])
+            
+        self.render('map.html', mode='single', data=data)
 
 class SummitsHandler(web.RequestHandler):
     def initialize(self, mode):
         self.mode = mode
 
     def get(self, prefix):
-        max_n = int(self.get_argument('max', 1000))
+        max_n = int(self.get_argument('max', 5000))
 
-        summits = filter(lambda e: e['geo'].startswith(prefix), {'up': index_up, 'down': index_down}[self.mode])
-        summits = sorted(summits, key=lambda e: e['prom'], reverse=True)[:max_n]
-        if summits:
-            print 'threshold: %s' % summits[-1]['prom']
+        def hex_limit(prefix, upper):
+            if not prefix:
+                return 2**63 - 1 if upper else -2**63
+            
+            dec = int((prefix + (('f' if upper else '0')*16))[:16], 16)
+            if dec >= 2**63:
+                dec -= 2**64
+            return dec
+
+        geo_min = hex_limit(prefix, False)
+        geo_max = hex_limit(prefix, True)
+        
+        results = conn.execute('''
+          select point, AsWkt(loc), elev_mm, prom_mm, min_bound
+          from prom join points on (prom.point = points.geocode)
+          where point between ? and ?
+          order by prom_mm desc
+          limit ?;
+        ''', (geo_min, geo_max, max_n))
+        results = list(results)
+        
+        def to_prom(row): 
+            return {
+                'point': geo_itos(row[0]),
+                'prom': row[3] / 1000.,
+                'min_bound': bool(row[4]),
+            }
+        def to_point(row):
+            return {
+                'geo': geo_itos(row[0]),
+                'coords': row[1],
+                'elev': row[2] / 1000.,
+            }
+        info = {
+            'proms': dict((e['point'], e) for e in map(to_prom, results)),
+            'points': dict((e['geo'], e) for e in map(to_point, results)),
+        }
+        if results:
+            print 'threshold: %s' % to_prom(results[-1])['prom']
 
         data = {
             'type': 'FeatureCollection',
-            'features': [summit_feature(s) for s in summits],
+            'features': [summit_feature(info, geo) for geo in info['proms'].keys()],
         }
 
         self.render('map.html', mode='many', data=data)
 
-def _load_points(mode):
-    i = 0
-    while True:
-        i += 1
-        path = os.path.join(settings.dir_out, '_index_%s_%d' % (mode, i))
-        if not os.path.exists(path):
-            break
-        with open(path) as f:
-            for p in json.load(f):
-                if mode == 'down' and p['elev'] == 0:
-                    # spurious?
-                    continue
-                yield p
-
-def load_points(mode, maxnum):
-    if not maxnum:
-        index = list(_load_points(mode))
-    else:
-        maxnum *= 1000
-        chunksize = max(.2 * maxnum, 50000)
-
-        index = []
-        for chunk in util.chunker(_load_points(mode), chunksize):
-            index.extend(chunk)
-            index.sort(key=lambda e: e['prom'], reverse=True)
-            index = index[:maxnum]
-
-    print 'loaded (%d)' % len(index)
-    return index
-
 if __name__ == "__main__":
 
     parser = OptionParser()
-    parser.add_option("-m", "--max", dest="max", type='int',
-                  help="only load <max> thousand greatest points")
-    parser.add_option("-d", "--dir", dest="dir", help="load from directory")
     (options, args) = parser.parse_args()
 
+    dbpath = args[0]
+    
     try:
-        port = int(args[0])
+        port = int(args[1])
     except IndexError:
         port = 8000
 
-    if options.dir:
-        settings.dir_out = options.dir
-
+    conn = sqlite3.connect(dbpath)
+    conn.enable_load_extension(True)
+    conn.load_extension("mod_spatialite")
+    
     application = web.Application([
         (r'/view/(?P<tag>.*)', MapViewHandler),
         (r'/summits/(?P<prefix>[0-9a-f]*)', SummitsHandler, {'mode': 'up'}),
@@ -220,9 +242,6 @@ if __name__ == "__main__":
         (r'/(.*)', web.StaticFileHandler, {'path': 'static'}),
     ], template_path='templates', debug=True)
     application.listen(port)
-
-    index_up = load_points('up', options.max)
-    index_down = load_points('down', options.max)
 
     try:
         IOLoop.instance().start()
