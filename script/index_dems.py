7936dcd2abd5d37d0865a326eccce35181138ffc
Drew Roos <mrgriscom@gmail.com>
2019-02-06 17:28:36 +0200

    clean up pole capping

--- script/index_dems.py
+++ script/index_dems.py
@@ -28,25 +28,32 @@ def transform_px(transform, px, py):
     return (tx[0] + tx[1]*px + tx[2]*py,
             tx[3] + tx[4]*px + tx[5]*py)
 
+def lon_epoch(x):
+    return int(math.floor(x / 360. + .5))
+
+def normalize_epoch(bound, epoch):
+    world = box(-180, -90, 180, 90)
+    return translate(bound, -360 * epoch, 0).intersection(world)
+
 def non_geo_bound(srs, x0, y0, x1, y1, tolerance):
-    LL_EPSILON = 1e-5
-    geo = osr.SpatialReference()
-    geo.ImportFromEPSG(4326)
+    LL_EPSILON = 1e-7
     def make_tx(src, dst):
         transform = osr.CoordinateTransformation(src, dst)
         def tx(p):
             try:
                 return list(transform.TransformPoint(*p)[:2])
             except:
-                return None
+                return [float('nan')]*2
         return tx
+    geo = osr.SpatialReference()
+    geo.ImportFromEPSG(4326)
     tx = make_tx(srs, geo)
     inv_tx = make_tx(geo, srs)
     midp = lambda p0, p1: [(a+b)/2. for a, b in zip(p0, p1)]
-    dist = lambda p0, p1: sum((a-b)**2. for a, b in zip(p0, p1))**.5 if p0 and p1 else float('+inf')
+    dist = lambda p0, p1: sum((a-b)**2. for a, b in zip(p0, p1))**.5
     lonnorm = lambda x: (x + 180.) % 360. - 180.
     is_polar = lambda p: abs(p[1]) > 90. - LL_EPSILON
-    is_antip = lambda p0, p1: abs(lonnorm(p0[0] - p1[0])) > 180. - LL_EPSILON
+    is_lon_oppo = lambda p0, p1: abs(lonnorm(p0[0] - p1[0])) > 180. - LL_EPSILON
     
     def yield_seg(start, end):
         # progressively sub-divide a line segment until the midpoint in lat/lon space closely approximates
@@ -56,19 +63,19 @@ def non_geo_bound(srs, x0, y0, x1, y1, tolerance):
         llend = tx(end)
         llmid = None
         polar = True
-        extra_points = []
+        pole_discontinuities = []
         # for pole crossings, use a lat/lon midpoint on a (possibly discontinuous) vertical line directly
         # to/from the pole. emit the segment to close the discontinuity as necessary
         if is_polar(llstart):
             llstart[0] = llend[0]
         elif is_polar(llend):
             llend[0] = llstart[0]
-            extra_points = [llend]
-        elif is_antip(llstart, llend):
-            sign = 1 if llstart[1] + llend[1] > 0 else -1
-            lat_delta = (llstart[1] - llend[1]) * sign / 2.
-            llmid = ((llend if lat_delta > 0 else llstart)[0], sign * (90 - abs(lat_delta)))
-            extra_points = [(llstart[0], sign * 90), (llend[0], sign * 90)]
+            pole_discontinuities = [llend]
+        elif is_lon_oppo(llstart, llend):
+            north = 1 if llstart[1] + llend[1] > 0 else -1
+            lat_delta = (llstart[1] - llend[1]) * north / 2.
+            llmid = ((llend if lat_delta > 0 else llstart)[0], north * (90 - abs(lat_delta)))
+            pole_discontinuities = [(p[0], north * 90) for p in (llstart, llend)]
         else:
             polar = False
         # don't compute lat/lon midpoint if already computed with special logic above
@@ -81,8 +88,8 @@ def non_geo_bound(srs, x0, y0, x1, y1, tolerance):
             dist(mid, inv_tx(llmid)) < tolerance or
             (not polar and dist(mid, inv_tx(llmid_idl)) < tolerance)):
             yield llstart
-            for ep in extra_points:
-                yield ep
+            for p in pole_discontinuities:
+                yield p
         else:
             for p in yield_seg(start, mid):
                 yield p
@@ -93,44 +100,43 @@ def non_geo_bound(srs, x0, y0, x1, y1, tolerance):
     segs = [(e, corners[(i + 1) % len(corners)]) for i, e in enumerate(corners)]
     poly = list(itertools.chain(*(yield_seg(*seg) for seg in segs)))
 
+    def rel_append(poly, points):
+        for p in points:
+            # normalize for IDL wraparound by setting lon to +/- 180 of the previous point's lon
+            last_lon = poly[-1][0]
+            if is_lon_oppo(p, poly[-1]) and is_polar(p):
+                # special handling for pole discontinuity segments - move in the direction of the original poly start point
+                delta_lon = math.copysign(180, poly[0][0] - last_lon)
+            else:
+                delta_lon = lonnorm(p[0] - last_lon)
+            poly.append((last_lon + delta_lon, p[1]))
+    
     poly_wrapped = poly[:1]
-    for p in poly[1:]:
-        # normalize for IDL wraparound by setting lon to +/- 180 of the previous point's lon
-        last_lon = poly_wrapped[-1][0]
-        delta_lon = lonnorm(p[0] - last_lon)
-        # special handling for pole discontinuity segments - move in the direction of the original poly start point
-        if abs(delta_lon) > 180. - LL_EPSILON and is_polar(p):
-            delta_lon = math.copysign(180, poly_wrapped[0][0] - last_lon)
-        poly_wrapped.append((last_lon + delta_lon, p[1]))
+    rel_append(poly_wrapped, poly[1:])
 
     # handle pole caps
     winding = int(round(abs(poly_wrapped[0][0] - poly_wrapped[-1][0]) / 360.))
     assert winding <= 1, 'double wind'
     if winding == 1:
-        poly_wrapped = [(lonnorm(p[0]), p[1]) for p in poly_wrapped]
-        prev_ix = lambda i: (i-1) % len(poly_wrapped)
-        for i in xrange(len(poly_wrapped)):
-            if abs(poly_wrapped[i][0] - poly_wrapped[prev_ix(i)][0]) > 180.:
-                discont_ix = i
+        proj_np = inv_tx((0, 90))
+        north = 1 if (x0 <= proj_np[0] <= x1) and (y0 <= proj_np[1] <= y1) else -1
+
+        # repeat ring until we fully span a [-180,180] interval
+        while True:
+            rel_append(poly_wrapped, poly)
+            min_epoch, max_epoch = [lon_epoch(f(p[0] for p in poly_wrapped)) for f in (min, max)]
+            if max_epoch - min_epoch >= 2:
                 break
-        start = poly_wrapped[prev_ix(discont_ix)]
-        end = poly_wrapped[discont_ix]
-        lon_k = lonnorm(180 - start[0]) / lonnorm(end[0] - start[0])
-        cross_lat = start[1] * (1 - lon_k) + end[1] * lon_k
-        sign = math.copysign(1, start[0])
-        pole_lat = math.copysign(90, cross_lat) # not 100% robust
-        poly_wrapped[discont_ix:discont_ix] = [(sign*180, cross_lat), (sign*180, pole_lat), (-sign*180, pole_lat), (-sign*180, cross_lat)]
+
+        # add the pole cap and normalize lons
+        poly_wrapped.extend((poly_wrapped[i][0], north * 90) for i in (-1, 0))
+        return normalize_epoch(Polygon(poly_wrapped), min_epoch + 1)
 
     return Polygon(poly_wrapped)
 
 def idl_normalize_bound(bound):
-    def slices():
-        world = box(-180, -90, 180, 90)
-        xmin, _, xmax, _ = bound.bounds
-        to_epoch = lambda x: int(math.floor(x / 360. + .5))
-        for epoch in xrange(to_epoch(xmin), to_epoch(xmax) + 1):
-            yield translate(bound, -360 * epoch, 0).intersection(world)
-    return reduce(lambda a, b: a.union(b), slices())
+    xmin, _, xmax, _ = bound.bounds
+    return reduce(lambda a, b: a.union(b), (normalize_epoch(bound, epoch) for epoch in xrange(lon_epoch(xmin), lon_epoch(xmax) + 1)))
 
 def process_series(root, _glob, sidecars, series):
     for path in sorted(glob.glob(os.path.join(root, _glob))):
@@ -195,13 +201,13 @@ def process_dem(root, path, sidecars, series):
     grid['subpx_offset'] = [round(k % 1., SUBPX_PREC) for k in grid0]
     dem['origin'] = [int(math.floor(k)) for k in grid0]
 
-    # TODO handle bounds for other projections -- include 1/2 pixel buffer and subdivide lines until projection error is < epsilon*pixel
     x0, y0 = transform_px(gt, 0, 0)
     x1, y1 = transform_px(gt, dem['width'], dem['height'])
     if grid['srs'] == 'epsg:4326':
         bound = box(x0, y0, x1, y1)
     else:
-        bound = non_geo_bound(srs, x0, y0, x1, y1, 0.1 * grid['spacing'])
+        tolerance = 0.1
+        bound = non_geo_bound(srs, x0, y0, x1, y1, tolerance * grid['spacing'])
     dem['bound'] = idl_normalize_bound(bound).wkt
         
     band = ds.GetRasterBand(1)
